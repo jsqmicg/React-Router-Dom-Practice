6366a31e50baa8e3e36599e12042c3f538bcc526
jsqmicg <jsquinonezm@correo.udistrital.edu.co>
2021-09-27 17:43:46 -0500

    creating public routes, and hiding with authtentication

--- src/routers/PrivateRoutes.js
+++ src/routers/PrivateRoutes.js
@@ -1,11 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import { Route, Redirect, useLocation } from "react-router";
-=======
-import { Route, Redirect } from "react-router";
->>>>>>> main
 import useAuth from "../auth/useAuth";
-import DashboardPage from "../pages/Dashboards";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const auth = useAuth();
