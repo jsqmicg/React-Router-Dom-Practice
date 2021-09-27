88b6cbf40208d845c4eff8b93a9f68c1d08aa297
jsqmicg <jsquinonezm@correo.udistrital.edu.co>
2021-09-27 16:29:46 -0500

    resolving conflicts

--- src/routers/PrivateRoutes.js
+++ src/routers/PrivateRoutes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router";
-<<<<<<< HEAD
 import useAuth from "../auth/useAuth";
 import DashboardPage from "../pages/Dashboards";
 
@@ -10,16 +9,6 @@ function PrivateRoute({ component: Component, ...rest }) {
     <Route {...rest}>
       {auth.user ? <Component /> : <Redirect to={"/login"} />}
     </Route>
-=======
-import DashboardPage from "../pages/Dashboards";
-
-//const user = null;
-const user = { id: 1, username: "basstian94" };
-
-function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route {...rest}>{user ? <Component /> : <Redirect to={"/"} />}</Route>
->>>>>>> main
   );
 }
 
