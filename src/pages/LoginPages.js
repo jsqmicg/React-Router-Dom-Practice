69f24a218afb3b1f98e5bc629e47fa5d02de0c21
Sebastián Quiñónez <80492686+jsqmicg@users.noreply.github.com>
2021-09-27 17:23:54 -0500

    Redirect to last page (#4)
    * private_routes
    
    * authprovider is created to emule a user login
    
    * resolving conflicts
    
    * redirecting to last page

--- src/pages/LoginPages.js
+++ src/pages/LoginPages.js
@@ -1,13 +1,16 @@
 import React from "react";
 import useAuth from "../auth/useAuth";
-import { useHistory } from "react-router";
+import { useHistory, useLocation } from "react-router";
 
 function LoginPages() {
   const history = useHistory();
+  const location = useLocation();
   const auth = useAuth();
+  const previousObjectURL = location.state?.from;
   const handleLogin = () => {
     auth.login();
-    history.push("/dashboard");
+    history.push(previousObjectURL || "/dashboard");
+    console.log(location);
   };
   const handleLogout = () => auth.logout();
   return (
--- src/routers/PrivateRoutes.js
+++ src/routers/PrivateRoutes.js
@@ -1,13 +1,22 @@
 import React from "react";
+<<<<<<< HEAD
+import { Route, Redirect, useLocation } from "react-router";
+=======
 import { Route, Redirect } from "react-router";
+>>>>>>> main
 import useAuth from "../auth/useAuth";
 import DashboardPage from "../pages/Dashboards";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const auth = useAuth();
+  const location = useLocation();
   return (
     <Route {...rest}>
-      {auth.user ? <Component /> : <Redirect to={"/login"} />}
+      {auth.user ? (
+        <Component />
+      ) : (
+        <Redirect to={{ pathname: "/login", state: { from: location } }} />
+      )}
     </Route>
   );
 }
